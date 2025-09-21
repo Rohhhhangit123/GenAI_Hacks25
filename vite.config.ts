4ba6faabd37a125ce09eadb171cf4120bee50a33
Rohhhhangit123 <149319194+Rohhhhangit123@users.noreply.github.com>
2025-09-21 19:13:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/configg/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
